d548b2950faed10627a5a068847d844ca459c2ec
hasanisu <hasanjimam@gmail.com>
2024-01-26 18:16:53 +0900

    first commit

---
+++ src/components/Container.tsx
@@ -0,0 +1,9 @@
+const Container = ({ children }) => {
+  return (
+    <div className="w-full h-full max-w-[1220px] px-[20px] mx-auto ">
+      {children}
+    </div>
+  );
+};
+
+export default Container;
---
+++ src/pages/Home.tsx
@@ -0,0 +1,11 @@
+import Container from "@/components/Container";
+
+const Home = () => {
+  return (
+    <Container>
+      <h1>This is Home Componets</h1>
+    </Container>
+  );
+};
+
+export default Home;
