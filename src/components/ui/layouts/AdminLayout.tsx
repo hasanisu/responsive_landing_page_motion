cfc70bf8b273ed053e23a45d48b31dee67460c36
hasanisu <hasanjimam@gmail.com>
2024-01-31 18:18:52 +0900

    added routes

---
+++ src/components/ui/layouts/AdminLayout.tsx
@@ -0,0 +1,15 @@
+import { Outlet } from "react-router-dom";
+import Sideber from "./Sideber";
+
+const AdminLayout = () => {
+  return (
+    <div className="grid grid-cols-12">
+      <Sideber />
+      <div className="col-span-10 h-[200vh]">
+        <Outlet />
+      </div>
+    </div>
+  );
+};
+
+export default AdminLayout;
---
+++ src/pages/admin/ServiceList.tsx
@@ -0,0 +1,9 @@
+const ServiceList = () => {
+  return (
+    <div>
+      <h2>this is service list</h2>
+    </div>
+  );
+};
+
+export default ServiceList;
